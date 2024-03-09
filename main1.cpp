d231372156366ee0e82b53a9cd5b19a225f61f52
NidaSamreen <152636590+NidaSamreen@users.noreply.github.com>
2024-03-09 23:52:28 +0530

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -1,7 +1,7 @@
-include <iostream>
+#include <iostream>
 
 int main() {
     // Print "Hello, World!" to the console
-    std::cout << "Hello, World! << std::endl;
+    std::cout << "Hello, World! "<< std::endl;
     return 0;
 }
